85af0f282f2245e996b29df9fee1af856437d075
tomseimandi <tom.seimandi@gmail.com>
2023-11-21 13:59:05 +0000

    Add tests for data/utils.py

--- tests/data/test_utils.py
+++ tests/data/test_utils.py
@@ -1,8 +1,13 @@
 """
 Tests for astrovision/data/utils.py
 """
-from astrovision.data.utils import generate_tiles_borders
+from astrovision.data.utils import (
+    generate_tiles_borders,
+    get_bounds_for_tile,
+    get_transform_for_tile,
+)
 from collections import Counter
+from affine import Affine
 
 
 def test_generate_tiles_borders():
@@ -18,3 +23,55 @@ def test_generate_tiles_borders():
         ((1, 2), (0, 1)),
     ]
     assert Counter(tiles) == Counter(awaited_tiles)
+
+
+def test_generate_tiles_borders_overlap():
+    height = 4
+    width = 3
+    tile_length = 2
+    tiles = generate_tiles_borders(height, width, tile_length)
+
+    awaited_tiles = [
+        ((0, 2), (0, 2)),
+        ((2, 4), (0, 2)),
+        ((0, 2), (1, 3)),
+        ((2, 4), (1, 3)),
+    ]
+    assert Counter(tiles) == Counter(awaited_tiles)
+
+
+def test_generate_tiles_borders_large_image():
+    height = 6
+    width = 4
+    tile_length = 2
+    tiles = generate_tiles_borders(height, width, tile_length)
+
+    awaited_tiles = [
+        ((0, 2), (0, 2)),
+        ((2, 4), (0, 2)),
+        ((4, 6), (0, 2)),
+        ((0, 2), (2, 4)),
+        ((2, 4), (2, 4)),
+        ((4, 6), (2, 4)),
+    ]
+    assert Counter(tiles) == Counter(awaited_tiles)
+
+
+def test_get_bounds_for_tile():
+    transform = Affine.translation(1, 5)
+    row_indices = (1, 2)
+    col_indices = (0, 1)
+    bounds = get_bounds_for_tile(transform, row_indices, col_indices)
+
+    awaited_bounds = (1, 7, 2, 6)
+    assert bounds == awaited_bounds
+
+
+def test_get_transform_for_tile():
+    transform = Affine.translation(1, 1)
+    col = 2
+    row = 2
+
+    tile_transform = get_transform_for_tile(transform, row, col)
+    awaited_tile_transform = Affine.translation(3, 3)
+    assert tile_transform == awaited_tile_transform
