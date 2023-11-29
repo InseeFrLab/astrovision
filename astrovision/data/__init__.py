d803f268a27c21c829280bc8601553a178df2aea
tomseimandi <tom.seimandi@gmail.com>
2023-11-29 17:09:40 +0000

    Modify imports

--- astrovision/data/__init__.py
+++ astrovision/data/__init__.py
@@ -1,9 +1,7 @@
 """
 Data module.
 """
-
 from .satellite_image import SatelliteImage
+from .labeled_satellite_image import SegmentationLabeledSatelliteImage
 
-__all__ = [
-    "SatelliteImage",
-]
+__all__ = ["SatelliteImage", "SegmentationLabeledSatelliteImage"]
