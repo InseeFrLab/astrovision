22cb0141844d9c520674dca2bb6cb922b9388892
tomseimandi <tom.seimandi@gmail.com>
2023-11-29 18:03:37 +0100

    Bump gdal (#12)
    * Update gdal version

--- install_gdal.sh
+++ install_gdal.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
 sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo apt-get update
-sudo add-apt-repository -y ppa:ubuntugis/ppa
-sudo apt-get update
-sudo apt-get install -y gdal-bin
-sudo apt-get install -y libgdal-dev
+sudo apt-get install python3-gdal -y
+sudo apt-get install libgdal-dev -y
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
