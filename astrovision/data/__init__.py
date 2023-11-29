06d9a85028d58d5dd22c5033bc0657af95dafba0
Thomas Faria <57811152+ThomasFaria@users.noreply.github.com>
2023-11-29 15:29:34 +0100

    add labeled image class

--- astrovision/data/__init__.py
+++ astrovision/data/__init__.py
@@ -3,7 +3,9 @@ Data module.
 """
 
 from .satellite_image import SatelliteImage
+from .satellite_image import SegmentationLabeledSatelliteImage
 
 __all__ = [
     "SatelliteImage",
+    "SegmentationLabeledSatelliteImage"
 ]
