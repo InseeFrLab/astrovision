63a32ee90d8cd1658a5490f3aa21d8964d78e228
tomseimandi <tom.seimandi@gmail.com>
2023-11-30 14:56:34 +0000

    Hotfix available imports

--- astrovision/data/__init__.py
+++ astrovision/data/__init__.py
@@ -2,6 +2,15 @@
 Data module.
 """
 from .satellite_image import SatelliteImage
-from .labeled_satellite_image import SegmentationLabeledSatelliteImage
+from .labeled_satellite_image import (
+    SegmentationLabeledSatelliteImage,
+    DetectionLabeledSatelliteImage,
+    ClassificationLabeledSatelliteImage,
+)
 
-__all__ = ["SatelliteImage", "SegmentationLabeledSatelliteImage"]
+__all__ = [
+    "SatelliteImage",
+    "SegmentationLabeledSatelliteImage",
+    "DetectionLabeledSatelliteImage",
+    "ClassificationLabeledSatelliteImage",
+]
