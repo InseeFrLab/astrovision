9a40d42f854d79a695c3e28d21b8609e06f039ae
tomseimandi <tom.seimandi@gmail.com>
2023-12-11 09:08:33 +0000

    Add python3-numpy install prior to GDAL install

--- install_gdal.sh
+++ install_gdal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get update
+sudo apt-get install python3-numpy -y
 sudo apt-get install python3-gdal -y
 sudo apt-get install libgdal-dev -y
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
