7908def3660ea830ccccd08efade17518a5faadb
tomseimandi <tom.seimandi@gmail.com>
2024-01-12 17:40:51 +0000

    Add tutorial notebook

--- astrovision/filter/__init__.py
+++ astrovision/filter/__init__.py
@@ -1,3 +1,8 @@
 """
 Filter module
 """
+from .corruption import filter_corrupted
+from .bounds import filter_oob
+from .clouds import filter_cloudy
+
+__all__ = ["filter_corrupted", "filter_oob", "filter_cloudy"]
